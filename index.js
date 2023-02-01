2d645f5e9eeb8fc08bc4fd283e05c1435e496a1f
Rishi <l.rishissahu@gmail.com>
2023-02-01 17:40:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// import express from 'express';
+// import exphbs from 'express-handlebars';
+
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define the root route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+'/home.html');
+});
+
+// Define the add-user route
+app.get('/add-user', (req, res) => {
+  res.sendFile(__dirname+'/add-user.html');
+});
+
+// Define the assign-task route
+app.get('/assign-task', (req, res) => {
+  res.sendFile(__dirname+'/assign-task.html');
+});
+
+
+app.post('/add-user', (req, res) => {
+  const { name, email, mobile } = req.body;
+  const query = `INSERT INTO users (name, email, mobile) VALUES ('${name}', '${email}', '${mobile}')`;
+
+  connection.query(query, (error, results) => {
+    if (error) throw error;
+    res.send('User added successfully');
+  });
+});
+
+app.post('/assign-task', function (req, res) {
+  // Collect task details from request
+  var user_id = req.body.user_id;
+  var task_name = req.body.task_name;
+  var task_type = req.body.task_type;
+
+  // Insert task details into tasks table
+  connection.query('INSERT INTO tasks (user_id, task_name, task_type) VALUES (?, ?, ?)', [user_id, task_name, task_type], function (error, results, fields) {
+    if (error) throw error;
+    res.send('Task assigned succesfully successfully');
+  });
+});
+
+
+// Start the server and listen on a specific port
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
+
+// Create a database connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456789',
+  database: 'task_app'
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database: ', err);
+    return;
+  }
+  console.log('Connected to database');
+});
